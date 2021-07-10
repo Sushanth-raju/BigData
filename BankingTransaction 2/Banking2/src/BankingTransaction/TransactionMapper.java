43646a16d75057cfbf561a41af657fc9c48eecbd
Sushanth Raju <57227895+Sushanth-raju@users.noreply.github.com>
2021-07-10 13:41:21 +0530

    Add files via upload

---
+++ BankingTransaction 2/Banking2/src/BankingTransaction/TransactionMapper.java
@@ -0,0 +1,19 @@
+package BankingTransaction;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+
+public class TransactionMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+	//private final static IntWritable one = new IntWritable(1);
+
+	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+
+		String valueString = value.toString();
+		String[] Transaction = valueString.split(",");
+		output.collect(new Text(Transaction[2]), new IntWritable(Integer.parseInt(Transaction[3])));
+	}
+}
